73921c5fd8248024aa05926fc06fa83f22e280dd
stevenGYG <steven.verlinden@getyourguide.com>
2024-10-14 11:42:13 +0200

    Add project structure and mock code

---
+++ background.js
@@ -0,0 +1,12 @@
+const hoveredTab = () => {
+    // implement a method that holds the hovering logic
+    // returns the hovered tab and potentially the content that needs to be summarized. 
+}
+
+const summaryGenerator = () => {
+    // this method will engage with the ai and return a  summary of the tab
+}
+
+const summaryDisplayer = () => {
+    // this will hold the logic to display the summarized info.
+}
